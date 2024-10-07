f9c66e03b094382535f5e6028b43b02bf7cedb5c
rjtkumar@github <rjtkumardev@gmail.com>
2024-10-08 01:46:05 +0530

    Scrape the next page recursively with scrapy-playwright

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -17,13 +17,30 @@ class QuotesSpider(scrapy.Spider):
             "errback" : self.errback
         })
 
-    def parse(self, response):
+    async def parse(self, response):
+        # now that we'll be scrapingg multiple pages we should also close pages to conserve memory
+        page = response.meta["playwright_page"]
+        await page.close()
+
         for quote in response.css("div.quote"):
             quote_item = QuoteItem()
             quote_item["text"] = quote.css("span.text::text").get()
             quote_item["author"] = quote.css("small.author::text").get()
             quote_item["tags"] = quote.css("div.tags a.tag::text").get()
             yield quote_item
+        
+        # getting the next page
+        next_page = response.css(".next>a::attr(href)").get()
+        if next_page is not None:
+            next_page_url = "https://quotes.toscrape.com" + next_page
+            yield scrapy.Request(next_page_url,meta={
+                "playwright": True,
+                "playwright_include_page" : True,
+                "playwright_Page_methods" : [
+                    PageMethod("wait_for_selector", "div.quote")
+                ],
+                "errback" : self.errback
+            })
     
     async def errback (self, failure):
         page = failure.request.meta["playwright_page"]
