54834c097fec866e5a7334dc2f3309904f135f29
rjtkumar@github <rjtkumardev@gmail.com>
2024-10-07 22:46:38 +0530

    Tell scrapy-playwright to wait for the page to finish loading before calling parse() Waits for certain elements to appear on the page

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -1,14 +1,21 @@
 from typing import Iterable
 import scrapy
 from quotes_js_scraper.items import QuoteItem
-
+from scrapy_playwright.page import PageMethod
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
     def start_requests(self):
         url = "https://quotes.toscrape.com/js/"
-        yield scrapy.Request(url, meta={"playwright": True})
+        yield scrapy.Request(url, meta={
+            "playwright": True,
+            "playwright_include_page" : True,
+            "playwright_Page_methods" : [
+                PageMethod("wait_for_selector", "div.quote")
+            ],
+            "errback" : self.errback
+        })
 
     def parse(self, response):
         for quote in response.css("div.quote"):
@@ -17,3 +24,7 @@ class QuotesSpider(scrapy.Spider):
             quote_item["author"] = quote.css("small.author::text").get()
             quote_item["tags"] = quote.css("div.tags a.tag::text").get()
             yield quote_item
+    
+    async def errback (self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
